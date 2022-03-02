a06b6b06de1641b5e349fbe4f2711e6cfe4238d5
AdventureBootCoder <captainthomasboots@gmail.com>
2022-03-02 13:08:03 -0800

    fixed powderkeg and naval mine

--- lua/entities/ent_jack_powderkeg/cl_init.lua
+++ lua/entities/ent_jack_powderkeg/cl_init.lua
@@ -2,7 +2,7 @@
 include('shared.lua')
 
 function ENT:Initialize()
-	self.Nice=ClientsideModel("models/props_trainyard/distillery_barrel001.mdl")
+	self.Nice=ClientsideModel("models/jmodels/explosives/bombs/powderkeg/powderkeg.mdl")
 	self.Nice:SetMaterial("models/entities/mat_jack_powderkeg")
 	self.Nice:SetPos(self:GetPos())
 	self.Nice:SetAngles(self:GetAngles())
@@ -12,7 +12,7 @@ end
 
 function ENT:Draw()
 	local Matricks=Matrix()
-	Matricks:Scale(Vector(.21,.21,.225))
+	Matricks:Scale(Vector(.80, .80, 1))
 	self.Nice:EnableMatrix("RenderMultiply",Matricks)
 	self.Nice:DrawModel()
 	--self.Entity:DrawModel()
